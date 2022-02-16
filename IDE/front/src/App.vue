1e7bdb8e30cb5b7cc68257b31fff08c1e1432c7f
Martin-Bruel <martin.bruel999@gmail.com>
2022-02-16 10:38:50 +0100

    setup IDE + setup editor

---
+++ IDE/front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="editor">
+    <Editor/>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor.vue'
+
+export default {
+  components: {
+    Editor
+  }
+}
+</script>
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+}
+#editor {
+  height: 50%;
+  width: 50%;
+}
+</style>
